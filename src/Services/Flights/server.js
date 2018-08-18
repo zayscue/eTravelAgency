a3f569c413b1cd571233b35cf6e6131e0c337b5f
Zackery Ayscue <ztayscue@Zackerys-MacBook-Pro.local>
2018-08-18 14:35:48 -0400

    add redis connection library.

--- src/Services/Flights/server.js
+++ src/Services/Flights/server.js
@@ -1,14 +1,26 @@
 'use strict';
 
 const express = require('express');
+const redis = require('redis');
+const {promisify} = require('util');
+
+const client = redis.createClient({
+    "host": "flights.data"
+});
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = express();
 
+const getAsync = promisify(client.get).bind(client);
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello world\n');
+    getAsync('foo').then(function(result) {
+        res.send(result);
+    });
 });
 
 app.listen(PORT, HOST);
